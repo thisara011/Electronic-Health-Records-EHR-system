b4df41bff10eca038a14b405065449d83f82672d
Deshan Lanka <119747677+deshanlankal@users.noreply.github.com>
2025-05-16 09:18:13 +0530

    .....

--- fruntend/src/components/Header.jsx
+++ fruntend/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="bg-white shadow-md py-4 px-6 flex justify-between items-center">
       {/* Logo + Name */}
       <div className="flex items-center space-x-2">
-        <img src="/logo.png" alt="MediLab Logo" className="h-8 w-8" />
+        <img src="/public/assets/logo.svg" alt="MediLab Logo" className="h-8 w-8" />
         <span className="text-xl font-bold text-purple-700">MediLab Hospital</span>
       </div>
 
